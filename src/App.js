7a05ef5830b01a3e90810000633236472e979c6d
Samantha Daugherty <samdaugherty26@gmail.com>
2021-08-19 18:06:29 -0400

    sign in screen

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndUp from './pages/sign-in-and-up/sign-in-and-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-up.styles.scss';
+
+const SignInAndUp = () => (
+    <div className='sign-in-and-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndUp;
