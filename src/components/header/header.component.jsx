0c59e433322637939bc57f59222c6150164c7ce4
Samantha Daugherty <samdaugherty26@gmail.com>
2021-08-20 18:01:51 -0400

    1st redux commit: setup, userReducer + userActions added to header + app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import {auth} from '../../firebase/firebase.utils';
 
@@ -25,4 +26,9 @@ const Header = ({currentUser}) => (
     </div>
 );
 
-export default Header;
+// use wherever you need to map state to props for redux
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/user/user.reducer.jsx
@@ -0,0 +1,17 @@
+
+// what you'd set for this.state in constructor in react
+const INITIAL_STATE = {currentUser: null};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state, 
+                currentUser: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
