58bb1f5f3b6977beda2e1e4cfeccdfb5d5ff245c
Samantha Daugherty <samdaugherty26@gmail.com>
2021-08-20 13:40:49 -0400

    sign up with email and password

--- src/pages/sign-in-and-up/sign-in-and-up.component.jsx
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-up.styles.scss';
 
 const SignInAndUp = () => (
     <div className='sign-in-and-up'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
