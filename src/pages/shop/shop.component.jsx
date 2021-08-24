bab27a0b98cf2a77ecbbd85ede5461603957f1d5
Samantha Daugherty <samdaugherty26@gmail.com>
2021-08-23 23:16:59 -0400

    removed unused imports

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-
-import {selectCollections} from '../../redux/shop/shop.selectors';
 
 const ShopPage = ({collections}) => (
     <div className='shop-page'>
