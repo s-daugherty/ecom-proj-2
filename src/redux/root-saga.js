2ec6af6e111dec5d2ee9a1150e016fccd4e9177a
Samantha Daugherty <samdaugherty26@gmail.com>
2021-09-08 21:55:39 -0500

    using sagas to clear cart on sign out

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -2,10 +2,12 @@ import {all, call} from 'redux-saga/effects';
 
 import {fetchCategoriesStart} from './shop/shop.sagas';
 import {userSagas} from './user/user.sagas';
+import { cartSagas } from './cart/cart.sagas';
 
 export default function* rootSaga() {
     yield all([
         call(fetchCategoriesStart),
-        call(userSagas)
+        call(userSagas),
+        call(cartSagas)
     ]);
 };
